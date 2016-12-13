4d0da98e1ccd0c89cf763102aa80cad30bc5ef7b
cogas <cheeky.chic.chick@gmail.com>
2016-12-13 17:58:41 +0900

    make vlaste_excption (move Exception subclass.)

---
+++ vlaste_exception.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+class DictionaryBuildError(Exception):
+    pass
+
+class WordBuildError(Exception):
+    pass
+
+class MetadataError(Exception):
+    pass
+
+class WordComponentsError(Exception):
+    pass
